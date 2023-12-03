4c847c94ed907184c755ef243236fbe52d12a1ba
nikitha6666 <128280339+Nikitha6666@users.noreply.github.com>
2023-12-03 20:43:24 +0530

    Update dfsadjmatrix.c

--- dfsadjmatrix.c
+++ dfsadjmatrix.c
@@ -1,78 +1,38 @@
-// 5.Create an adjacency matrix for graph and implement DFS.
-
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct node {
-    struct node *next;
-    int vertex;
-} node;
-
-node *G[20];
-int visited[20];
-int n;
-
-void read_graph();
-void insert(int, int);
+#include<stdio.h>
+ 
 void DFS(int);
-
-int main() {
-    int i;
-    read_graph();
-
-    for (i = 0; i < n; i++)
-        visited[i] = 0;
-
-    DFS(1);
-    printf("\n"); // Add a newline character to separate the output list from the next line
-    return 0;
-}
-
-void DFS(int i) {
-    node *p;
-    printf("%d ", i);
-    p = G[i];
-    visited[i] = 1;
-    while (p != NULL) {
-        i = p->vertex;
-        if (!visited[i])
-            DFS(i);
-        p = p->next;
-    }
-}
-
-void read_graph() {
-    int i, vi, vj, no_of_edges;
+int G[10][10],visited[10],n;    //n is no of vertices and graph is sorted in array G[10][10]
+ 
+void main()
+{
+    int i,j;
     printf("Enter number of vertices:");
-    scanf("%d", &n);
-
-    for (i = 0; i < n; i++) {
-        G[i] = NULL;
-        printf("Enter number of edges:");
-        scanf("%d", &no_of_edges);
-
-        for (i = 0; i < no_of_edges; i++) {
-            printf("Enter an edge(u,v):");
-            scanf("%d%d", &vi, &vj);
-            insert(vi, vj);
-            insert(vj, vi);
-        }
-    }
+   
+	scanf("%d",&n);
+ 
+    //read the adjacency matrix
+	printf("\nEnter adjecency matrix of the graph:");
+   
+	for(i=0;i<n;i++)
+       for(j=0;j<n;j++)
+			scanf("%d",&G[i][j]);
+ 
+    //visited is initialized to zero
+   for(i=0;i<n;i++)
+        visited[i]=0;
+ 
+    DFS(0);
 }
-
-void insert(int vi, int vj) {
-    node *p, *q;
-    q = (node *)malloc(sizeof(node));
-    q->vertex = vj;
-    q->next = NULL;
-
-    if (G[vi] == NULL)
-        G[vi] = q;
-    else {
-        p = G[vi];
-        while (p->next != NULL)
-            p = p->next;
-        p->next = q;
-    }
+ 
+void DFS(int i)
+{
+    int j;
+	printf("\n%d",i);
+    visited[i]=1;
+	
+	for(j=0;j<n;j++)
+       if(!visited[j]&&G[i][j]==1)
+            DFS(j);
 }
+ 
 
